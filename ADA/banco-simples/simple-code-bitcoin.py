c94e465a9a2c8f956cbba50bf13282ac759af052
zzzmiike <michael.silva@sptech.school>
2024-08-02 11:47:29 -0300

    Certificados

---
+++ ADA/banco-simples/simple-code-bitcoin.py
@@ -0,0 +1,37 @@
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, previous_hash, timestamp, data, hash):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.data = data
+        self.hash = hash
+
+def calculate_hash(index, previous_hash, timestamp, data):
+    value = str(index) + str(previous_hash) + str(timestamp) + str(data)
+    return hashlib.sha256(value.encode('utf-8')).hexdigest()
+
+def create_genesis_block():
+    return Block(0, "0", int(time.time()), "Genesis Block", calculate_hash(0, "0", int(time.time()), "Genesis Block"))
+
+def create_new_block(previous_block, data):
+    index = previous_block.index + 1
+    timestamp = int(time.time())
+    hash = calculate_hash(index, previous_block.hash, timestamp, data)
+    return Block(index, previous_block.hash, timestamp, data, hash)
+
+# Example usage
+blockchain = [create_genesis_block()]
+previous_block = blockchain[0]
+
+num_of_blocks_to_add = 10
+
+for i in range(0, num_of_blocks_to_add):
+    data = f"Block {i + 1} data"
+    block_to_add = create_new_block(previous_block, data)
+    blockchain.append(block_to_add)
+    previous_block = block_to_add
+    print(f"Block #{block_to_add.index} has been added to the blockchain!")
+    print(f"Hash: {block_to_add.hash}\n")
---
+++ ADA/while.py
@@ -0,0 +1,10 @@
+import numpy as np
+import random
+
+numberSorted = random.randint(1,10)
+x =  np.random.randint(1,52, (3,3))
+number = 0
+
+while numberSorted != number:
+    number = int(input('Insira um valor entre 0 e 10'))
+    print(numberSorted)
